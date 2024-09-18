52824857026978d2bec2992ecb6d98d21693ced1
AbhishekSelacoti <110489413+AbhishekSelacoti@users.noreply.github.com>
2024-09-18 16:54:08 +0530

    Removed secrets

--- challenge2/challenge2.js
+++ challenge2/challenge2.js
@@ -3,15 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    type: 'OAuth2',
-    user: 'abhishekselacoti9@gmail.com',
-    pass: 'ya29.a0AcM612xu6Tr1fpGyzSyA6Lub1KLHFIBmI_0yUcIsQbrPB_kE3pFw9m4lI8p2PCf88SgaTfVsXAxzWeA9KX_mdAOf-KYb02cchRD1Ulf4wGeGEMQttk2xQhSmbZ8aXLlQQcldvg49ZvalhbA6J1G6WpJsYTcgJpIehxXoNpnYaCgYKAacSARMSFQHGX2Migx7UnsSBmW3ZVDZn1AIYAA0175',
-    clientId: '267515917616-o1ibscnks1h8av0mosbj7mt2ut69p60k.apps.googleusercontent.com',
-    clientSecret: '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',
-    refreshToken: '1//04xSm8w6fj-9YCgYIARAAGAQSNwF-L9Ir04KtAaUBPOhXOeIPDUX1VfIyd7kRq-lDmL46BHvS7ZAH39R_K29_FHKb7HXg_RS9s_g'
-
-
-    // Kindly uncomment and add your credentials to run the code
+    // Kindly copy paste a code which has been sent over mail and then run the program
   }
 });
 
